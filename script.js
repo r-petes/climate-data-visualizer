b0fe6cbe52d45dbd82909b1701c5ed8dff2e1bcf
r-petes <rachel.peterson.5683@gmail.com>
2021-09-17 13:17:47 -0600

    added state/county JSON data for http request lookup

---
+++ script.js
@@ -0,0 +1,17 @@
+
+// Drop down menus for the year range
+    // Populate based on the API data (from our own JSON file?)
+
+// Drop down menu for selecting the county/city 
+    // Populate based on the API data (pull from our own JSON file?)
+
+// Once the user has filled out both the year range and county/city data, call the API (using Fetch?)
+
+// Analyze that data 
+
+// Generate charts based on the data pulled (using what tools?)
+
+// Push the charts to html
+
+
+
