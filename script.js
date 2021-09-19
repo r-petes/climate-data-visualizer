9102fb66f295bb05657517242ce58742907ebc04
r-petes <rachel.peterson.5683@gmail.com>
2021-09-19 14:58:14 -0600

    formatting fixed

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 
-// TO DO: Drop down menus for the year range? 
-    // Populate based on the API data (from our own JSON file)
-
 // Populate the state dropdown from our JSON, the id of each state will be the FIPS code (used in the API for data retrieval)
 
 document.addEventListener('DOMContentLoaded', function () {
